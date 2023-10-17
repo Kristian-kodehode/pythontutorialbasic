59c1131269b9754a4e5d8edb6ffb3c3aca82b200
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-17 14:54:10 +0200

    udpated, need to get the formattr to work

--- lesson2.py
+++ lesson2.py
@@ -1,7 +1,7 @@
 line01 = "********************" # Header / footer
 line02 = "*                  *" # re-use
 line03 = "*     WELCOME!     *"
-line04 = "helllo"
+
 # Starts with a blank line
 print('')
 print(line01)
@@ -9,4 +9,5 @@ print(line02)
 print(line03)
 print(line02)
 print(line01)
+
 
